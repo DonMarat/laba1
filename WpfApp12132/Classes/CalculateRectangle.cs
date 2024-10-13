53dfe148901e9ca7207f5a78b8d77591cff596e9
DonMarat <marat_marv_2005@mail.ru>
2024-10-13 20:44:33 +0300

    Сделал модульные тесты

--- WpfApp12132/Classes/CalculateRectangle.cs
+++ WpfApp12132/Classes/CalculateRectangle.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace WpfApp12132.Classes
 {
-    class CalculateRectangle : Icalculatorintegral
+    public class CalculateRectangle : Icalculatorintegral
 
     {
         public double Calculate(double lower, double upper, int count, Func<double, double> func)
         {
+            if (lower > upper)
+                throw new ArgumentException("Lower are greater than Upper");
+
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count) + "is not positive");
+
+            if (func == null)
+                throw new ArgumentException("Function is null");
+
             double h = (upper - lower) / count;
             double sum = 0.0;
             for (int i = 0; i < count; i++)
--- WpfApp12132/Classes/Calculatetrap.cs
+++ WpfApp12132/Classes/Calculatetrap.cs
@@ -3,17 +3,22 @@ using System.Linq;
 
 namespace WpfApp12132.Classes
 {
-    class Calculatetrap: Icalculatorintegral
+    public class Calculatetrap: Icalculatorintegral
     {
         public double Calculate(double lower, double upper, int count, Func<double, double> func)
         {
+            if (lower > upper)
+                throw new ArgumentException("Lower are greater than Upper");
+
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count) + "is not positive");
+
+            if (func == null)
+                throw new ArgumentException("Function is null");
             double h = (upper - lower) / count;
-            double sum = 0.0;
-            for (int i = 0; i < count; i++)
-            {
-                sum += func(lower + i * h);
-            }
-            return h * sum;
+            double sum = Enumerable.Range(0, count).Sum(s => func(s * h + lower));
+
+            return h * ((func(upper) + func(lower)) / 2 + sum);
         }
     }
 }
